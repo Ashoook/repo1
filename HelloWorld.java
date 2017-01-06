eaa2106578d0a1c0400a54464902ccfc17b2899c
Ashoook <ashok.velpula@gmail.com>
2017-01-06 15:15:25 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, Worldnew12345");
+      System.out.println("Hello, Worldnew7788");
    }
 }
